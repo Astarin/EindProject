5d058e7b4e14056c1969b05113328b0e7ce6ac86
HagarDeVerschrikkelijke <hagar.deverschrikkelijke@belgacom.net>
2015-02-13 13:52:39 +0100

    Startpagina MVC toegevoegd

---
+++ EindProjectMVC/Controllers/VerlofController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EindProjectMVC.Controllers
+{
+    public class VerlofController : Controller
+    {
+        //
+        // GET: /Verlof/
+        public ActionResult Index()
+        {
+            return View("StartPagina");
+        }
+	}
+}
