f59d5211da02727b9db2fa95e70321bc7ca44a50
Roel <roel_vercauteren@hotmail.com>
2015-02-13 10:30:10 +0100

    Klasse VerlofAanvraag toegevoegd aan Model

---
+++ EindProjectBusinessModels/VerlofAanvraag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EindProjectBusinessModels
+{
+    public class VerlofAanvraag
+    {
+        public DateTime StartDatum { get; set; }
+        public DateTime EindDatum { get; set; }
+        public DateTime AanvraagDatum { get; set; }
+        public string Toestand { get; set; }
+        public int Personeelsnr { get; set; }
+    }
+}
