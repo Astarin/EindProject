6863b56b56f0c1274cb5c5b8bd60e7bee0a4d4e3
dnuyts <david.nuyts@gmail.com>
2015-02-16 16:42:00 +0100

    VerwijderTeam toegevoegd

--- EindProjectDAL/DalMethodes.cs
+++ EindProjectDAL/DalMethodes.cs
@@ -180,13 +180,53 @@ namespace EindProjectDAL
 
         /*****************************
          * 1.2.3. Opvragen van teams *
-         *****************************/
+         *****************************
+         * David 15/02/15            *
+         *****************************/ 
+        public List<Team> OpvragenTeams(string code, string teamnaam, string teamleader)
+        {
+            //  De medewerker geeft 0, 1 of meer van volgende criteria op:
+            //   - Gedeelte van teamnaam
+            //   - Gedeelte van naam van teamverantwoordelijke
+            //   - Code
+            // Het systeem toont de gegevens (code; naam; nummer, naam en voornaam teamverantwoordelijke;
+            // nummer naam en voornaam van alle werknemers die tot het team behoren ) van de teams die aan
+            // alle opgegeven criteria voldoen.  De gegevens zijn gesorteerd op teamnaam.  Binnen een team
+            // zijn de gegevens van de werknemers gesorteerd op naam en voornaam van de werknemers.
+            return null;
+        }
+
+
 
 
         /**********************************
          * 1.2.4 Verwijderen van een team *
          **********************************/
-
+        public void VerwijderTeam(Team team)
+        {
+            using (DbEindproject db = new DbEindproject())
+            {
+                var wn = from w in db.Werknemers
+                         where w.Team.Code == team.Code
+                         select w;
+                if (wn != null)
+                {
+                    throw new Exception("Er bestaan nog werknemers in dit team.");
+                }
+                else
+                {
+                    try
+                    {
+                        db.Teams.Remove(team);
+                        db.SaveChanges();
+                    }
+                    catch
+                    {
+                        throw new Exception("Het verwijderen van het team is niet succesvol verlopen.");
+                    }
+                }
+            }
+        }
 
         /*********************************
          * 1.2.5. Opvragen van Teamleden *
