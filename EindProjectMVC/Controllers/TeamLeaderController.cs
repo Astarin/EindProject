8e0dd9b09c0f7dd3ea83d6dd245cf997c96e33db
HagarDeVerschrikkelijke <hagar.deverschrikkelijke@belgacom.net>
2015-02-17 13:32:22 +0100

    Updated VerlofAanvraag / Fixed goedkeuren verlofaanvraag

--- EindProjectMVC/Controllers/TeamLeaderController.cs
+++ EindProjectMVC/Controllers/TeamLeaderController.cs
@@ -20,32 +20,44 @@ namespace EindProjectMVC.Controllers
             List<Werknemer> wnList = Dal.GeefTeamleden(team);
 
             var qry = from w in wnList
-                      select new SelectListItem { 
-                          Text = String.Format ("{0} {1} {2}",w.PersoneelsNr.ToString(),w.Naam,w.Voornaam), 
-                          Value = w.PersoneelsNr.ToString() };
+                      select new SelectListItem
+                      {
+                          Text = String.Format("{0} {1} {2}", w.PersoneelsNr.ToString(), w.Naam, w.Voornaam),
+                          Value = w.PersoneelsNr.ToString()
+                      };
             //ViewBag.ddlTeamLeden = new SelectList(wnList, "PersoneelsNr", "PersoneelsNr");
             ViewBag.ddlTeamLeden = qry.ToList();
             return View();
 
         }
 
-        public ActionResult InfoForWerknemer(int ddlTeamLeden)
+        public ActionResult InfoForWerknemer(int? ddlTeamLeden)
         {
+            if (ddlTeamLeden == null)
+            {
+                if (Session["personeelsNr"] != null) ddlTeamLeden = (int)Session["personeelsNr"];
+            }
             DalMethodes dal = new DalMethodes();
             Werknemer werknemer = (dal.VraagWerknemerOp(ddlTeamLeden.ToString(), "", "")).FirstOrDefault();
-
+            Session["werknemer"] = werknemer;
             return View(werknemer);
         }
 
-        public ActionResult UpdateStatusVerlofAanvraag(String Id)
+        public ActionResult UpdateStatusVerlofAanvraag(VerlofAanvraag v)
         {
             // Id is de id van de verlofaanvraag.
-            // To Do: 
-            // 1) de werknemer mee ophalen waarbij de verlofaanvraag hoort
-            // 2) de methode WijzigStatusVerlofaanvraag in DAL aanpassen zodat de wijziging van de verlofaanvraag
-            //    ook de werknemer als input vraagt. (Twee verschillende werknemers kunnen beiden een verlofaanvraag
-            //    met vb. Id=1 hebben !!
-            return View("Index");
+            Werknemer werknemer;
+            DalMethodes dal = new DalMethodes();
+            dal.WijzigStatusVerlofaanvraag(v, Aanvraagstatus.Goedgekeurd);
+
+            if (Session["werknemer"] != null) 
+            {
+                werknemer = (Werknemer)Session["werknemer"];
+                werknemer = (dal.VraagWerknemerOp(werknemer.PersoneelsNr.ToString(), "", "")).FirstOrDefault();
+            }
+            else { werknemer = null; }
+
+            return View("InfoForWerknemer", werknemer);
         }
 
     }
