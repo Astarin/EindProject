70c4707ecd507623051a2101ed103ca1d77b6f1a
Astarin <berndvertommen@msn.com>
2015-02-18 15:54:45 +0100

    HrMenugenerator voorzien van link o mteamverantwoordelijken te beheren

--- EindProjectMVC/Models/HrMenuGenerator.cs
+++ EindProjectMVC/Models/HrMenuGenerator.cs
@@ -5,12 +5,12 @@ using System.Web;
 
 namespace EindProjectMVC.Models
 {
-   static  public class HrMenuGenerator
+    static public class HrMenuGenerator
     {
-       public static string CreeerHrMenu()
-       {
-           return
-           @"    <div class='jumbotron'>
+        public static string CreeerHrMenu()
+        {
+            return
+            @"    <div class='jumbotron'>
         <h2>Hr Selecteer Werknemer</h2>
         <nav class='navbar navbar-inverse'>
             <form action='/HR/HrNieuweWerknemer' class='label label-primary' >
@@ -25,10 +25,13 @@ namespace EindProjectMVC.Models
             <form action='/HR/HrTeamToevoegen' class='label label-danger'>
                 <input name='Actie' type='submit' value='Team Toevoegen' class='button-invis'>
             </form>
+            <form action='/HR/HrTeamVerantwoordelijkeBeheren' class='label label-primary'>
+                <input name='Actie' type='submit' value='Team Bewerken Toevoegen' class='button-invis'>
+            </form>
 
         </nav>
         </div>
            ";
-       }
+        }
     }
 }
