e3786baf71dbd57bb69da347f9acd02f1949d5a1
Astarin <berndvertommen@msn.com>
2015-02-18 15:18:27 +0100

    Views Hr toegevoegd : teamverantwoordleijken

--- EindProjectMVC/Controllers/HRController.cs
+++ EindProjectMVC/Controllers/HRController.cs
@@ -30,10 +30,18 @@ namespace EindProjectMVC.Controllers
             }
             else
             {
-                if (TeamLeaderControle(werknemer, team))
+                try
                 {
-                    methode.VoegWerknemerToeAanDb(werknemer, int.Parse(team));
+                    if (TeamLeaderControle(werknemer, team))
+                    {
+                        methode.VoegWerknemerToeAanDb(werknemer, int.Parse(team));
+                    }
                 }
+                catch (Exception exc)
+                {
+                    //todo iet met de exc.Message
+                }
+                
             }
 
             return View();
@@ -45,34 +53,36 @@ namespace EindProjectMVC.Controllers
             return View(werknemers);
         }
 
-        public ActionResult HrWijzigWerknemer(int? werknemerId)
-        {
-            NieuweTeamslijstAanmaken();
-            if (werknemerId == null)
-            {
-                //TODO ERROR
-            }
-            Werknemer werknemer = methode.VraagWerknemerOp(werknemerId.ToString(), "", "")[0]; // geef de 0 en normaal enige terug
-            return View(werknemer);
-        }
+        //public ActionResult HrWijzigWerknemer(int? werknemerId)
+        //{
+        //    NieuweTeamslijstAanmaken();
+        //    if (werknemerId == null)
+        //    {
+        //        //TODO ERROR
+        //    }
+        //    Werknemer werknemer = methode.VraagWerknemerOp(werknemerId.ToString(), "", "")[0]; // geef de 0 en normaal enige terug
+        //    return View(werknemer);
+        //}
         [HttpPost]
         public ActionResult HrWijzigWerknemer(Werknemer werknemer, string team)
         {
             NieuweTeamslijstAanmaken();
-            if (TeamLeaderControle(werknemer, team))
+            try
             {
-                methode.WijzigWerknemerProperty(werknemer, int.Parse(team));
+                if (TeamLeaderControle(werknemer, team))
+                {
+                    methode.WijzigWerknemerProperty(werknemer, int.Parse(team));
+                }
             }
+            catch (Exception exc)
+            {
+                //todo iet met de exc.Message
+            }
+           
             return View(werknemer);
         }
 
-        public ActionResult HrWJaarlijksVerlofToevoegen()
-        {
-
-            return View();
-        }
-
-        public ActionResult HrTeamToevoegen(Team team)
+             public ActionResult HrTeamToevoegen(Team team)
         {
             if (team.Naam != String.Empty && team.Naam != null)
             {
@@ -81,6 +91,17 @@ namespace EindProjectMVC.Controllers
 
             return View();
         }
+
+             public ActionResult HrWJaarlijksVerlofToevoegen()
+             {
+                 return View();
+             }
+
+        public ActionResult HrTeamVerantwoordelijkeBeheren()
+             {
+                 return View();
+             }
+
         private void NieuweTeamslijstAanmaken()
         {
             if (TeamsNieuweWerknemer == null)
